3cb5c07d2f347b39345d575ea306f926e23c8e70
balancejia <balancejia@qq.com>
2018-11-13 09:09:38 +0800

    format

--- src/jia/leetCode/TwoSum.java
+++ src/jia/leetCode/TwoSum.java
@@ -2,40 +2,40 @@ package jia.leetCode;
 
 /**
  * @author 贾红磊
- * 
- *         Given an array of integers, return indices of the two numbers such
- *         that they add up to a specific target.
- * 
- *         You may assume that each input would have exactly one solution, and
- *         you may not use the same element twice
+ * <p>
+ * Given an array of integers, return indices of the two numbers such
+ * that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and
+ * you may not use the same element twice
  */
 public class TwoSum {
 
-	public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
 
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
 
-				if (nums[i] + nums[j] == target) {
-					return new int[] { i, j };
-				}
-			}
-		}
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		int[] nums = { 2, 7, 11, 15 };
-		int target = 9;
-		
-		int[] result= twoSum(nums,target);
-		
-		System.out.println(result[0]+","+result[1]);
-		
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(result[0] + "," + result[1]);
+
+    }
 
 }
